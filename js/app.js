ffd84591250967f8bad437a14185647f187293a7
PPProsenjite <prosenjitpappu03455@gmial.com>
2022-09-03 06:58:46 +0600

    add spinner

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const loadCategories = async() =>{
+const loadAllCategories = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url);
     const data = await res.json();
@@ -7,15 +7,35 @@ const loadCategories = async() =>{
 const displayCategories = (categories) =>{
     const categoryContainer = document.getElementById('category-container');
    categories.forEach(category =>{
-    const {category_name} = category;
+    const {category_name, category_id} = category;
     const categoryDiv = document.createElement('div');
     categoryDiv.classList.add('col');
     categoryDiv.innerHTML = `
-    <a class = 'btn' >${category_name}</a>
+    <a class = 'btn' onclick="showcard('${category_id}')">${category_name}</a>
     `
     categoryContainer.appendChild(categoryDiv);
    })
-   
 
 }
-loadCategories();
+const selectCategory= async (value) => {
+    try {
+        const category = `https://openapi.programming-hero.com/api/news/category/${value}`
+        const res = await fetch(category)
+        const data = await res.json();
+        displayCategriOne(data.data);
+
+    } catch (e) {
+        console.log("some error");
+
+    }
+
+
+}
+const showcard = (category_id) => {
+    loadingSpinner(true);
+    categorieOne(category_id);
+
+}
+selectCategory();
+loadAllCategories();
+
